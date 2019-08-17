8082f5f5cbcd236eda0b310402ab54e0b77cba87
Vivek Hanasoge <46605922+vivekh99@users.noreply.github.com>
2019-08-16 22:13:14 -0400

    app_folder_commit

---
+++ src/main/java/com/example/screenshotapplication/Screenshot.java
@@ -0,0 +1,28 @@
+package com.example.screenshotapplication;
+
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Screenshot {
+    public static Bitmap takescreenshot(View v) {
+        v.setDrawingCacheEnabled(true);
+        v.buildDrawingCache(true);
+        Bitmap b = Bitmap.createBitmap(v.getDrawingCache());
+        v.setDrawingCacheEnabled(false);
+        return b;
+    }
+
+    public static Bitmap takescreenshotofRootView(View v) {
+        return takescreenshot(v.getRootView());
+    }
+}
+
+
+
